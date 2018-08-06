5e46dd13c19d864215760a541a98a29a391cca53
sandytan10 <stan10@stuy.edu>
2018-08-06 18:23:14 +0000

    automatically backed up by learn

--- prac.rb
+++ prac.rb
@@ -2,4 +2,4 @@ first_name = "Sandy"
 last_name = "Tan"
 age = 15
 # String Interpolation
-puts "My first name is #{first_name} and i am #{age}"
+puts "My first name is #{first_name} and i am #{age.to_s}"
